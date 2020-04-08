1709256a994f0607fa5af3efaff810b70b16ce1d
kauaipalama <kauaipalama@gmail.com>
2020-04-07 19:04:39 -0700

    Replaced label with textView and and allowed for hyperlink attributed text in interface builder. Removed textContainerInset from textView.

--- ZenTimer/View/CardViewController.swift
+++ ZenTimer/View/CardViewController.swift
@@ -12,7 +12,10 @@ class CardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        footerTextView.textContainerInset = UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
+        footerTextView.textContainer.lineFragmentPadding = 0
     }
     
     @IBOutlet weak var handleView: UIView!
+    @IBOutlet weak var footerTextView: UITextView!
 }
