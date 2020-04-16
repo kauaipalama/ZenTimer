a067097322d0b374727657b0412373f5f6a518ad
kauaipalama <kauaipalama@gmail.com>
2020-04-15 22:20:30 -0700

    Implemented logic for requestReview in ReviewController. Requests review once after one week and one more timee a year after that. Able to changed numberOfRequests condition in shouldRequestReview in order to obtain further reviews if needed.

--- ZenTimer/Controller/ReviewController.swift
+++ ZenTimer/Controller/ReviewController.swift
@@ -10,11 +10,52 @@ import Foundation
 import StoreKit
 
 class ReviewController {
-    fileprivate init() {}
+    private init() {}
     
     static let shared = ReviewController()
     
+    private var initialLaunch: Date? {
+        get {
+            return UserDefaults.standard.value(forKey: "initialLaunch") as? Date
+        }
+        set {
+            return UserDefaults.standard.set(newValue, forKey: "initialLaunch")
+        }
+    }
+    
+    private var numberOfRequests: Int {
+        get {
+            return UserDefaults.standard.integer(forKey: "numberOfRequests")
+        }
+        set {
+            return UserDefaults.standard.set(newValue, forKey: "numberOfRequests")
+        }
+    }
+    
+    private var oneWeekAgo: Date {
+        return Calendar.current.date(byAdding: .day, value: -7, to: Date())!
+    }
+    
+    private var oneHundredTwentyThreeDaysLater: Date {
+        return Calendar.current.date(byAdding: .day, value: -365, to: Date())!
+    }
+    
+    private var shouldRequestReview: Bool {
+        if initialLaunch == nil {
+            initialLaunch = Date()
+            return false
+        } else if let initialLaunch = self.initialLaunch, initialLaunch < oneWeekAgo && numberOfRequests < 1 {
+            numberOfRequests += 1
+            return true
+        } else if let initialLaunch = self.initialLaunch, initialLaunch < oneHundredTwentyThreeDaysLater && numberOfRequests < 2 {
+            numberOfRequests += 1
+            return true
+        }
+        return false
+    }
+    
     func requestReview() {
+        guard  shouldRequestReview else {return}
         if PDTimerController.shared.pdTimer.audioSettingsState == .soundOff {
             DispatchQueue.main.async {
                 SKStoreReviewController.requestReview()
